ac2755cf6b70dbec0b3e0346807f92541af13f62
sp2254 <sp2254@njit.edu>
2017-02-12 14:14:22 -0500

    php file updated

--- display_discount.php
+++ display_discount.php
@@ -14,3 +14,30 @@
   $discount_percent_formatted = $discount_percent."%";
   $discount_formatted = "$".number_format($discount, 2);
   $discount_price_formatted = "$".number_format($discount_price, 2);  
+
+  // escape the unformatted input
+  $product_description_escaped = htmlspecialchars($product_description);
+  ?>
+
+ <!DOCTYPE html>
+   <html>
+    <head>
+    <title>Product Discount Calculator</title>
+   <link rel="stylesheet" type="text/css" href="main.css">
+   </head>
+   <body>
+  <main>
+ <h1>Product Discount Calculator</h1>
+  <label>Product Description:</label>
+  <span><?php echo $product_description_escaped; ?></span><br>
+ <label>List Price:</label>
+ <span><?php echo $list_price_formatted; ?></span><br>
+ <label>Standard Discount:</label>
+  <span><?php echo $discount_percent_formatted; ?></span><br>
+ <label>Discount Amount:</label>
+ <span><?php echo $discount_formatted; ?></span><br>
+  <label>Discount Price:</label>
+  <span><?php echo $discount_price_formatted; ?></span><br>
+  </main>
+  </body>
+  </html>
